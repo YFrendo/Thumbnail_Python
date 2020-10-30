b09218631a6a44bbadef7c48ffbd78b9bbac230b
Frendo Yann <yann.frendo@live.fr>
2020-10-30 17:44:57 +0100

    Tout marche sauf l'affichage de la vignette en HTML mais ca on verra demain!

--- serveur_flask.py
+++ serveur_flask.py
@@ -1,13 +1,9 @@
-import json
 import os
-import pickle
-from flask import Flask,flash,request,redirect,url_for
-from werkzeug.utils import secure_filename
+from flask import Flask,flash,request,redirect,url_for, jsonify
 from PIL import Image, ExifTags
-from task_celery import extract_metadata
+from task_celery import vignette
 
 UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = {'pdf','png','jpg'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -35,6 +31,8 @@ def upload():
 
         id_dossier = str(len(os.listdir('./uploads'))) #Donne un numéroID unique
         os.makedirs('./uploads/' + id_dossier)#Fait un dossier avec le numéro d'id
+
+        filename, ext  = os.path.splitext(upload_file.filename)
         im.save(os.path.join(app.config['UPLOAD_FOLDER'] + '/' + id_dossier, upload_file.filename)) 
 
         exif = {
@@ -43,12 +41,37 @@ def upload():
                 if k in ExifTags.TAGS
                 }
         exif = str(exif)
-        print(type(exif))
-        print(exif)
-        with open('./uploads/' + id_dossier + '/' + upload_file.filename + '_metadata.json','w') as outfile:
+        with open('./uploads/' + id_dossier + '/metadata.json','w') as outfile:
             outfile.write(exif)
-        #Enregistre le fichier dans le dossier
+
+        vignette.delay(id_dossier,upload_file.filename)
         return id_dossier 
+
+@app.route('/images/<id_photo>')
+def image(id_photo):
+
+    with open('./uploads/' + id_photo + '/metadata.json','r') as outfile:
+        texte = outfile.read()
+        texte = str(texte) + '<br>'
+
+    if os.path.isfile('./uploads/' + id_photo + '/thumbnail.jpg'):
+        texte = texte + 'Thumbnail succes <br>'
+        texte = texte + 'http://127.0.0.1:5000/thumbnail/' + id_photo
+        return(texte)
+    elif os.path.isfile('./uploads/' + id_photo + '/fail'):
+        texte = texte + 'Echec création vignette'
+        return(texte)
+    texte = texte + 'Vignette en création'
+    return(texte)
+
+@app.route('/thumbnail/<id_photo>')
+def thumbnail(id_photo):
+
+    if os.path.isfile('./uploads/' + id_photo + '/thumbnail.jpg'):
+        thumbnail = './uploads/' + id_photo + '/thumbnail.jpg'
+        return('<img src="' + thumbnail +'">')
+    else:
+        return('Pas de vignette')
 
 
 
