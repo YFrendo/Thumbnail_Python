a618d4cc614b02a4a781ba71315e0b9055a3b432
Frendo Yann <yann.frendo@live.fr>
2020-11-01 14:47:24 +0100

    Ca fonctionne mais il reste à faire les doc annexe et quelques détails

--- serveur_flask.py
+++ serveur_flask.py
@@ -1,11 +1,15 @@
 import os
-from flask import Flask,flash,request,redirect,url_for, jsonify
+from flask import Flask,flash,request,redirect,url_for, jsonify, render_template
 from PIL import Image, ExifTags
 from task_celery import vignette
 
-UPLOAD_FOLDER = './uploads'
+UPLOAD_FOLDER = './static/uploads/'
+HTML_PATH  = '/static/uploads'
+HOST = 'http://127.0.0.1:5000'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['HOST'] = HOST
+app.config['HTML_PATH'] = HTML_PATH
 
 @app.route('/')
 def bienvenue():
@@ -16,6 +20,10 @@ def bienvenue():
 
 @app.route('/images',methods=['GET','POST'])
 def upload():
+
+    if not os.path.isfile(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+
     if request.method == 'POST':
         if 'file' not in request.files:
             return 'No file part'
@@ -29,11 +37,11 @@ def upload():
         except:
             return "Cannot open file"
 
-        id_dossier = str(len(os.listdir('./uploads'))) #Donne un numéroID unique
-        os.makedirs('./uploads/' + id_dossier)#Fait un dossier avec le numéro d'id
+        id_dossier = str(len(os.listdir(app.config['UPLOAD_FOLDER']))) #Donne un numéroID unique
+        os.makedirs(app.config['UPLOAD_FOLDER'] + id_dossier)#Fait un dossier avec le numéro d'id
 
         filename, ext  = os.path.splitext(upload_file.filename)
-        im.save(os.path.join(app.config['UPLOAD_FOLDER'] + '/' + id_dossier, upload_file.filename)) 
+        im.save(os.path.join(app.config['UPLOAD_FOLDER'] + id_dossier, upload_file.filename)) 
 
         exif = {
                 ExifTags.TAGS[k] : v
@@ -41,7 +49,7 @@ def upload():
                 if k in ExifTags.TAGS
                 }
         exif = str(exif)
-        with open('./uploads/' + id_dossier + '/metadata.json','w') as outfile:
+        with open(app.config['UPLOAD_FOLDER'] + id_dossier + '/metadata.json','w') as outfile:
             outfile.write(exif)
 
         vignette.delay(id_dossier,upload_file.filename)
@@ -50,26 +58,26 @@ def upload():
 @app.route('/images/<id_photo>')
 def image(id_photo):
 
-    with open('./uploads/' + id_photo + '/metadata.json','r') as outfile:
+    with open(app.config['UPLOAD_FOLDER'] + id_photo + '/metadata.json','r') as outfile:
         texte = outfile.read()
         texte = str(texte) + '<br>'
 
-    if os.path.isfile('./uploads/' + id_photo + '/thumbnail.jpg'):
+    if os.path.isfile(app.config['UPLOAD_FOLDER'] + id_photo + '/thumbnail.jpg'):
         texte = texte + 'Thumbnail succes <br>'
-        texte = texte + 'http://127.0.0.1:5000/thumbnail/' + id_photo
+        texte = texte + app.config['HOST'] + '/vignette/' + id_photo
         return(texte)
-    elif os.path.isfile('./uploads/' + id_photo + '/fail'):
+    elif os.path.isfile(app.config['UPLOAD_FOLDER'] + id_photo + '/fail'):
         texte = texte + 'Echec création vignette'
         return(texte)
     texte = texte + 'Vignette en création'
     return(texte)
 
-@app.route('/thumbnail/<id_photo>')
+@app.route('/vignette/<id_photo>')
 def thumbnail(id_photo):
 
-    if os.path.isfile('./uploads/' + id_photo + '/thumbnail.jpg'):
-        thumbnail = './uploads/' + id_photo + '/thumbnail.jpg'
-        return('<img src="' + thumbnail +'">')
+    if os.path.isfile(app.config['UPLOAD_FOLDER'] + '/'+  id_photo + '/thumbnail.jpg'):
+        full_filename = os.path.join(app.config['HTML_PATH'] + '/' + id_photo, 'thumbnail.jpg')
+        return(render_template("vignette.html", user_image = full_filename ))
     else:
         return('Pas de vignette')
 
