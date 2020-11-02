954a539a3bfb0eeedb1eb9e5dc5b86733ec4f8f4
Frendo Yann <yann.frendo@live.fr>
2020-11-02 11:21:25 +0100

    Bon bah tout est là plus qu'une petite relecture pour l'orthographe

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
 import serveur_flask
 import io
+import os
 
 class TestFlask(unittest.TestCase):
 
@@ -24,7 +25,7 @@ class TestFlask(unittest.TestCase):
             reponse = c.get('/blabladodo')
             self.assertEqual(reponse.status_code, 404)
 
-    def test_texte(self):
+    def test_image(self):
 
         with serveur_flask.app.test_client() as c:
 
@@ -34,6 +35,54 @@ class TestFlask(unittest.TestCase):
                     'file' : (imgIO, 'Image_test.jpg')
                     }
             reponse = c.post('/images' , data=data, content_type = 'multipart/form-data')
-            print(reponse.data)
             self.assertTrue(reponse.data.decode('UTF-8').isnumeric())
 
+    def test_texte(self):
+
+        with serveur_flask.app.test_client() as c:
+
+            with open('./image_test/fichier_format_texte.txt', 'rb') as img:
+                imgIO = io.BytesIO(img.read())
+            data = {
+                    'file' : (imgIO, 'Texte_test.txt')
+                    }
+            reponse = c.post('/images' , data=data, content_type = 'multipart/form-data')
+            self.assertEqual(reponse.data.decode('UTF-8'),'Cannot open file')
+
+    def test_crea_fichier(self):
+
+        with serveur_flask.app.test_client() as c:
+
+            with open('./image_test/Image_test.jpg', 'rb') as img:
+                imgIO = io.BytesIO(img.read())
+            data = {
+                    'file' : (imgIO, 'Image_test.jpg')
+                    }
+            c.post('/images' , data=data, content_type = 'multipart/form-data')
+            self.assertEqual(len(os.listdir('./static/uploads/0')),3)
+
+    def test_affiche_vignette(self):
+
+        with serveur_flask.app.test_client() as c:
+
+            with open('./image_test/Image_test.jpg', 'rb') as img:
+                imgIO = io.BytesIO(img.read())
+            data = {
+                    'file' : (imgIO, 'Image_test.jpg')
+                    }
+            c.post('/images' , data=data, content_type = 'multipart/form-data')
+            reponse = c.get('/vignette/0')
+            self.assertEqual(reponse.status_code, 200)
+
+    def test_vignette_nonexistante(self):
+
+        with serveur_flask.app.test_client() as c:
+
+            with open('./image_test/Image_test.jpg', 'rb') as img:
+                imgIO = io.BytesIO(img.read())
+            data = {
+                    'file' : (imgIO, 'Image_test.jpg')
+                    }
+            c.post('/images' , data=data, content_type = 'multipart/form-data')
+            reponse = c.get('/vignette/10000')
+            self.assertEqual(reponse.data.decode("UTF-8"),'Pas de vignette')
